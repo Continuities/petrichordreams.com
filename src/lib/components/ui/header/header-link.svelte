9551f04341d4fbe66f34a21326a2f18e0edea806
Michael Townsend <michael@doublespeakgames.com>
2026-03-27 19:52:55 -0400

    desktop menu

---
+++ src/lib/components/ui/header/header-link.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import type { ResolvedPathname } from '$app/types';
+	import { page } from '$app/state';
+
+	interface Props {
+		href: ResolvedPathname;
+		label: string;
+	}
+	let { href, label }: Props = $props();
+	let isActive = $derived(page.url.pathname === href);
+</script>
+
+<!-- eslint-disable-next-line svelte/no-navigation-without-resolve -->
+<a {href} class={isActive ? 'underline' : 'hover:underline opacity-80'}>
+	{label}
+</a>
--- src/lib/components/ui/header/header.svelte
+++ src/lib/components/ui/header/header.svelte
@@ -3,11 +3,12 @@
 	import { m } from '$lib/paraglide/messages';
 	import { Button } from '$lib/components/ui/button';
 	import { HugeiconsIcon } from '@hugeicons/svelte';
-	import { ShoppingBag01Icon } from '@hugeicons/core-free-icons';
+	import { ShoppingBag01Icon, Menu01Icon } from '@hugeicons/core-free-icons';
 	import { getCart } from '$lib/cart/index.svelte';
 	import Badge from '$lib/components/ui/badge/badge.svelte';
 	import { goto } from '$app/navigation';
 	import { resolve } from '$app/paths';
+	import HeaderLink from './header-link.svelte';
 
 	interface Props {
 		headerElement?: HTMLElement;
@@ -32,9 +33,26 @@
 		: undefined}
 >
 	<div class="flex justify-between items-center max-w-5xl mx-auto py-8 px-4 md:px-0">
+		<Button size="icon-lg" variant="ghost" class="md:hidden">
+			<HugeiconsIcon icon={Menu01Icon} class="size-6" />
+		</Button>
 		<a href={resolve('/')}>
 			<img src={logo} alt={m.logo()} class="h-12 w-12" />
 		</a>
+		<ul class="hidden md:flex gap-4 mr-auto ml-16">
+			<li>
+				<HeaderLink href={resolve('/')} label={m.menu_home()} />
+			</li>
+			<li>
+				<HeaderLink href={resolve('/custom')} label={m.menu_custom()} />
+			</li>
+			<li>
+				<HeaderLink href={resolve('/rituals')} label={m.menu_rituals()} />
+			</li>
+			<li>
+				<HeaderLink href={resolve('/about')} label={m.menu_about()} />
+			</li>
+		</ul>
 		<div class="flex">
 			<Button
 				size="icon-lg"

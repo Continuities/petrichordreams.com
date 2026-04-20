112f327e37fc984f9fe503a109e27c0405f43003
Michael Townsend <michael@doublespeakgames.com>
2026-04-20 13:04:51 -0400

    load shipping rates from Stripe instead of hardcoding ids

--- src/lib/stripe-service.ts
+++ src/lib/stripe-service.ts
@@ -18,148 +18,157 @@ type AllowedCountry = Stripe.Checkout.SessionCreateParams.ShippingAddressCollect
 
 const SHIPPING_COUNTRIES: AllowedCountry[] = ['CA']; // Just Canada for now
 
-const SHIPPING_RATES = [
-	'shr_1TFfCoBnG8CBNgzrB8gxSYIr', // Montreal pickup
-	'shr_1TFfCQBnG8CBNgzrqwfpt9T1' // Standard shipping
-];
-
 const stripe = new Stripe(STRIPE_SECRET_KEY);
 
 const getPrice = (product: Stripe.Product): CurrencyPrice => {
-	const price = product.default_price as Stripe.Price | undefined;
-	return {
-		CAD: (price?.unit_amount ?? 0) / 100
-	};
+  const price = product.default_price as Stripe.Price | undefined;
+  return {
+    CAD: (price?.unit_amount ?? 0) / 100
+  };
 };
 
 const getLocalisedValue = (product: Stripe.Product, key: string): string | null => {
-	const locale = getLocale();
-	const localizedKey = `${key}_${locale}`;
-	return product.metadata[localizedKey] || null;
+  const locale = getLocale();
+  const localizedKey = `${key}_${locale}`;
+  return product.metadata[localizedKey] || null;
 };
 
 const getArrayValue = (product: Stripe.Product, key: string): string[] => {
-	const currentLocale = getLocale();
-	const pattern = new RegExp(`^${key}(\\d+)_(${locales.join('|')})$`);
-	const value = Object.entries(product.metadata)
-		.map(([key, value]) => {
-			const match = key.match(pattern);
-			if (!match) {
-				return null;
-			}
-			const index = parseInt(match[1], 10);
-			const valueLocale = match[2];
-			if (valueLocale !== currentLocale) {
-				return null;
-			}
-			return { index, value };
-		})
-		.filter(Boolean) as { index: number; value: string }[];
-	value.sort((a, b) => a.index - b.index);
-	return value.map((item) => item.value);
+  const currentLocale = getLocale();
+  const pattern = new RegExp(`^${key}(\\d+)_(${locales.join('|')})$`);
+  const value = Object.entries(product.metadata)
+    .map(([key, value]) => {
+      const match = key.match(pattern);
+      if (!match) {
+        return null;
+      }
+      const index = parseInt(match[1], 10);
+      const valueLocale = match[2];
+      if (valueLocale !== currentLocale) {
+        return null;
+      }
+      return { index, value };
+    })
+    .filter(Boolean) as { index: number; value: string }[];
+  value.sort((a, b) => a.index - b.index);
+  return value.map((item) => item.value);
 };
 
 const productToPiece = (product: Stripe.Product): Piece => ({
-	id: product.id,
-	name: getLocalisedValue(product, 'name') ?? product.name,
-	price: getPrice(product),
-	sold: !product.active,
-	description: getLocalisedValue(product, 'description') ?? product.description ?? '',
-	mood: getLocalisedValue(product, 'mood') ?? '',
-	story: getArrayValue(product, 'story'),
-	images: [product.images[0], ...(product.metadata.images?.split(',') ?? [])],
-	heightCm: parseFloat(product.metadata.height_cm ?? '0'),
-	widthCm: parseFloat(product.metadata.width_cm ?? '0')
+  id: product.id,
+  name: getLocalisedValue(product, 'name') ?? product.name,
+  price: getPrice(product),
+  sold: !product.active,
+  description: getLocalisedValue(product, 'description') ?? product.description ?? '',
+  mood: getLocalisedValue(product, 'mood') ?? '',
+  story: getArrayValue(product, 'story'),
+  images: [product.images[0], ...(product.metadata.images?.split(',') ?? [])],
+  heightCm: parseFloat(product.metadata.height_cm ?? '0'),
+  widthCm: parseFloat(product.metadata.width_cm ?? '0')
 });
 
 export async function getPieces(active?: boolean): Promise<Piece[]> {
-	try {
-		const products = await stripe.products.list({ active, expand: ['data.default_price'] });
-		return products.data.filter((p) => p.metadata['hidden'] !== 'true').map(productToPiece);
-	} catch (error) {
-		console.error('Error fetching products:', error);
-		throw error;
-	}
+  try {
+    const products = await stripe.products.list({ active, expand: ['data.default_price'] });
+    return products.data.filter((p) => p.metadata['hidden'] !== 'true').map(productToPiece);
+  } catch (error) {
+    console.error('Error fetching products:', error);
+    throw error;
+  }
 }
 
 export async function getPiece(productId: string) {
-	try {
-		const product = await stripe.products.retrieve(productId, { expand: ['default_price'] });
-		return productToPiece(product);
-	} catch (error) {
-		console.error(`Error fetching product with ID ${productId}:`, error);
-		throw error;
-	}
+  try {
+    const product = await stripe.products.retrieve(productId, { expand: ['default_price'] });
+    return productToPiece(product);
+  } catch (error) {
+    console.error(`Error fetching product with ID ${productId}:`, error);
+    throw error;
+  }
+}
+
+export async function getShippingRates() {
+  try {
+    const rates = await stripe.shippingRates.list();
+    if (rates.data.length === 0) {
+      throw new Error('No shipping rates found');
+    }
+    return rates.data.map((rate) => ({ shipping_rate: rate.id }));
+  } catch (error) {
+    console.error('Error fetching shipping rates:', error);
+    throw error;
+  }
 }
 
 export async function createCheckoutSession(pieces: Piece[]): Promise<string> {
-	try {
-		const products = await Promise.all(
-			pieces.map((piece) => stripe.products.retrieve(piece.id, { expand: ['default_price'] }))
-		);
-		const session = await stripe.checkout.sessions.create({
-			ui_mode: 'embedded',
-			locale: getLocale(),
-			line_items: products.map((product) => ({
-				price: (product.default_price as Stripe.Price).id,
-				quantity: 1
-			})),
-			shipping_address_collection: {
-				allowed_countries: SHIPPING_COUNTRIES
-			},
-			shipping_options: SHIPPING_RATES.map((rate) => ({ shipping_rate: rate })),
-			mode: 'payment',
-			return_url: `${localisedResolve('/thanks')}?session_id={CHECKOUT_SESSION_ID}`
-		});
-		if (!session.client_secret) {
-			throw new Error('No client secret returned');
-		}
-		return session.client_secret;
-	} catch (error) {
-		console.error('Error creating checkout session:', error);
-		throw error;
-	}
+  try {
+    const products = await Promise.all(
+      pieces.map((piece) => stripe.products.retrieve(piece.id, { expand: ['default_price'] }))
+    );
+    const shippingRates = await getShippingRates();
+    const session = await stripe.checkout.sessions.create({
+      ui_mode: 'embedded',
+      locale: getLocale(),
+      line_items: products.map((product) => ({
+        price: (product.default_price as Stripe.Price).id,
+        quantity: 1
+      })),
+      shipping_address_collection: {
+        allowed_countries: SHIPPING_COUNTRIES
+      },
+      shipping_options: shippingRates,
+      mode: 'payment',
+      return_url: `${localisedResolve('/thanks')}?session_id={CHECKOUT_SESSION_ID}`
+    });
+    if (!session.client_secret) {
+      throw new Error('No client secret returned');
+    }
+    return session.client_secret;
+  } catch (error) {
+    console.error('Error creating checkout session:', error);
+    throw error;
+  }
 }
 
 export async function getCheckoutSession(sessionId: string) {
-	try {
-		const session = await stripe.checkout.sessions.retrieve(sessionId, {
-			expand: ['line_items.data.price.product']
-		});
-		return session;
-	} catch (error) {
-		console.error(`Error fetching checkout session with ID ${sessionId}:`, error);
-		throw error;
-	}
+  try {
+    const session = await stripe.checkout.sessions.retrieve(sessionId, {
+      expand: ['line_items.data.price.product']
+    });
+    return session;
+  } catch (error) {
+    console.error(`Error fetching checkout session with ID ${sessionId}:`, error);
+    throw error;
+  }
 }
 
 export async function getPurchasedPieces(sessionId: string): Promise<Piece[]> {
-	try {
-		const session = await getCheckoutSession(sessionId);
-		const pieces: Piece[] = session.line_items?.data
-			.map((item) => {
-				const product = item.price?.product;
-				return product ? productToPiece(product as Stripe.Product) : null;
-			})
-			.filter(Boolean) as Piece[];
-		return pieces;
-	} catch (error) {
-		console.error(`Error fetching purchased pieces for session ID ${sessionId}:`, error);
-		throw error;
-	}
+  try {
+    const session = await getCheckoutSession(sessionId);
+    const pieces: Piece[] = session.line_items?.data
+      .map((item) => {
+        const product = item.price?.product;
+        return product ? productToPiece(product as Stripe.Product) : null;
+      })
+      .filter(Boolean) as Piece[];
+    return pieces;
+  } catch (error) {
+    console.error(`Error fetching purchased pieces for session ID ${sessionId}:`, error);
+    throw error;
+  }
 }
 
 export async function markPiecesAsPurchased(pieces: Piece[]) {
-	try {
-		await Promise.all(
-			pieces.map((piece) =>
-				stripe.products.update(piece.id, {
-					active: false
-				})
-			)
-		);
-	} catch (error) {
-		console.error('Error marking pieces as purchased:', error);
-		throw error;
-	}
+  try {
+    await Promise.all(
+      pieces.map((piece) =>
+        stripe.products.update(piece.id, {
+          active: false
+        })
+      )
+    );
+  } catch (error) {
+    console.error('Error marking pieces as purchased:', error);
+    throw error;
+  }
 }
